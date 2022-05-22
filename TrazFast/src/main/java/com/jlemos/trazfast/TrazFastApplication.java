9489f38ec9838b96acb97c4479843be88fdc1e9b
João Lemos <derikbrik@gmail.com>
2022-05-21 22:36:54 -0300

    Inicial TrazFast

---
+++ TrazFast/src/main/java/com/jlemos/trazfast/TrazFastApplication.java
@@ -0,0 +1,13 @@
+package com.jlemos.trazfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrazFastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrazFastApplication.class, args);
+	}
+
+}
